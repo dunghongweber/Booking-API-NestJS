0a35bb02325ca6a4cf40fc2f6761070bf4c08960
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2022-06-13 04:34:02 +0700

    remove usermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { BookingsModule } from './bookings/bookings.module';
 import {MongooseModule} from "@nestjs/mongoose";
 
@@ -8,7 +7,6 @@ import {MongooseModule} from "@nestjs/mongoose";
 @Module({
   imports: [
     AuthModule,
-    UserModule,
     BookingsModule,
     MongooseModule.forRoot("mongodb+srv://dunghong123:pass123word456@cluster0.kv48y.mongodb.net/BookingApp?retryWrites=true&w=majority"),
   ],
